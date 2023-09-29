01323411613379191670f3aaeb00de360435feba
milosz08 <gilgamilosz451@gmail.com>
2023-09-29 15:49:24 +0200

    change env name of S3 static container

--- .vite/vite-docker-dev.config.mjs
+++ .vite/vite-docker-dev.config.mjs
@@ -9,7 +9,7 @@ import viteCommonConfig from './vite-common.config.mjs';
 export default viteCommonConfig({
   landingPageBaseUrl: `http://localhost:${process.env.ENV_MSPH_LANDING_PAGE_PORT}`,
   clientBaseUrl: `http://localhost:${process.env.ENV_MSPH_WEB_CLIENT_PORT}`,
-  cdnBaseUrl: `http://localhost:${process.env.ENV_MSPH_STATIC_S3_PORT}`,
+  cdnBaseUrl: `http://localhost:${process.env.ENV_MSPH_STATIC_S3_API_PORT}`,
   cdnBaseXHR: `http://msph-s3-static:9000`,
   isProdMode: true,
 });
--- .vite/vite-server.config.mjs
+++ .vite/vite-server.config.mjs
@@ -9,7 +9,7 @@ import viteCommonConfig from './vite-common.config.mjs';
 export default viteCommonConfig({
   landingPageBaseUrl: `http://localhost:${process.env.ENV_MSPH_LANDING_PAGE_DEV_PORT}`,
   clientBaseUrl: `http://localhost:${process.env.ENV_MSPH_WEB_CLIENT_DEV_PORT}`,
-  cdnBaseUrl: `http://localhost:${process.env.ENV_MSPH_STATIC_S3_PORT}`,
-  cdnBaseXHR: `http://localhost:${process.env.ENV_MSPH_STATIC_S3_PORT}`,
+  cdnBaseUrl: `http://localhost:${process.env.ENV_MSPH_STATIC_S3_API_PORT}`,
+  cdnBaseXHR: `http://localhost:${process.env.ENV_MSPH_STATIC_S3_API_PORT}`,
   isProdMode: false,
 });
